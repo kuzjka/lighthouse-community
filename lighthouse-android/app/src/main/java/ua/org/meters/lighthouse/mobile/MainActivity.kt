a24d9028ea48d807eeb49f4e37cc50c7b65542d3
Ihor Kuzmanenko <real.kuzjka@gmail.com>
2024-06-08 12:05:54 +0300

    added minimal setup to receive FCM notifications

--- lighthouse-android/app/src/main/java/ua/org/meters/lighthouse/mobile/MainActivity.kt
+++ lighthouse-android/app/src/main/java/ua/org/meters/lighthouse/mobile/MainActivity.kt
@@ -1,9 +1,15 @@
 package ua.org.meters.lighthouse.mobile
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import ua.org.meters.lighthouse.mobile.ui.theme.LighthouseTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +36,47 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        askNotificationPermission()
+        logRegistrationToken()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(baseContext, R.string.will_show_notifications, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(baseContext, R.string.will_not_show_notifications, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.d(TAG, "Notification permission already granted")
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                Toast.makeText(baseContext, R.string.notifications_needed, Toast.LENGTH_SHORT).show()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private fun logRegistrationToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Cannot get registration token", task.exception)
+                return@OnCompleteListener
+            }
+
+            Log.i(TAG, "Got FCM token: " + task.result)
+        })
+    }
+
+    companion object {
+        private const val TAG = "MainActivity";
     }
 }
 
