5f86448d59a32bf81d675b847e47daa4e4f8904d
Anton31 <anton30momot@gmail.com>
2024-06-09 11:18:09 +0300

    initial commit (#12)

---
+++ lighthouse-server/src/test/java/ua/org/meters/lighthouse/LighthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.org.meters.lighthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LighthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
