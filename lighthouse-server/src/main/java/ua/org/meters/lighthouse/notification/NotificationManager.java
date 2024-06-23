e38837531b63ff3ac9d3e1c0927e60437953756a
Ihor Kuzmanenko <real.kuzjka@gmail.com>
2024-06-23 19:54:40 +0300

    added Telegram bot notifier

--- lighthouse-server/src/main/java/ua/org/meters/lighthouse/notification/NotificationManager.java
+++ lighthouse-server/src/main/java/ua/org/meters/lighthouse/notification/NotificationManager.java
@@ -1,5 +1,7 @@
 package ua.org.meters.lighthouse.notification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import ua.org.meters.lighthouse.service.PowerEvent;
 
@@ -10,6 +12,8 @@ import java.util.List;
  * Listens to power state change events, chooses appropriate notification channels and send notification to users.
  */
 public class NotificationManager {
+    private static final Logger logger = LoggerFactory.getLogger(NotificationManager.class);
+
     private List<PowerNotifier> powerNotifiers = new ArrayList<>();
 
     /**
@@ -22,6 +26,12 @@ public class NotificationManager {
 
     @EventListener
     public void onPowerEvent(PowerEvent event) {
-        powerNotifiers.forEach(notifier -> notifier.notifyPower(event.isPowerOn()));
+        powerNotifiers.forEach(notifier -> {
+            try {
+                notifier.notifyPower(event.isPowerOn());
+            } catch (NotificationException e) {
+                logger.error("Notification failed", e);
+            }
+        });
     }
 }
