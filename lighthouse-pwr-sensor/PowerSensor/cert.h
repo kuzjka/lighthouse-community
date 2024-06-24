aca8681e77af482a724680642ab8e1082864954a
Ihor Kuzmanenko <real.kuzjka@gmail.com>
2024-06-24 20:13:44 +0300

    sensor: HTTPS and basic auth

---
+++ lighthouse-pwr-sensor/PowerSensor/cert.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const char fingerprint [] PROGMEM = "8C:08:F9:FA:80:95:09:CD:AB:3D:C9:53:9C:C5:D1:D0:5A:28:3D:C7";
--- lighthouse-pwr-sensor/PowerSensor/settings.h
+++ lighthouse-pwr-sensor/PowerSensor/settings.h
@@ -2,4 +2,5 @@
 #define WIFI_PASSWORD "password"
 #define SERVER_URL "http://lighthouse.meters.org.ua"
 #define REQUEST_RATE 30000
- 
+#define USERNAME "user"
+#define PASSWORD "pa33w0rd"
