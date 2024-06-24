5502f5e87f380f1467e2e526b5ac822c8b3d7dfa
Ihor Kuzmanenko <real.kuzjka@gmail.com>
2024-06-24 17:25:53 +0300

    added prd profile and Dockerfile

--- lighthouse-server/src/main/java/ua/org/meters/lighthouse/config/AppConfig.java
+++ lighthouse-server/src/main/java/ua/org/meters/lighthouse/config/AppConfig.java
@@ -2,6 +2,7 @@ package ua.org.meters.lighthouse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import ua.org.meters.lighthouse.notification.FCMNotifier;
 import ua.org.meters.lighthouse.notification.LoggingNotifier;
 import ua.org.meters.lighthouse.notification.NotificationManager;
@@ -17,11 +18,20 @@ public class AppConfig {
     }
 
     @Bean
+    @Profile("prd")
     public NotificationManager notificationManager(FCMNotifier fcmNotifier, TelegramNotifier telegramNotifier) {
         NotificationManager manager = new NotificationManager();
         manager.addPowerNotifier(new LoggingNotifier());
         manager.addPowerNotifier(fcmNotifier);
         manager.addPowerNotifier(telegramNotifier);
+        return manager;
+    }
+
+    @Bean
+    @Profile("!prd")
+    public NotificationManager notificationManagerDev() {
+        NotificationManager manager = new NotificationManager();
+        manager.addPowerNotifier(new LoggingNotifier());
         return manager;
     }
 }
